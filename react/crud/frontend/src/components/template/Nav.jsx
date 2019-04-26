532914b75d0a7b8100180b50c315dd88675949cb
glayner <thiagoglayner_tg@hotmail.com>
2019-04-26 15:34:12 -0300

    refatorando

--- react/crud/frontend/src/components/template/Nav.jsx
+++ react/crud/frontend/src/components/template/Nav.jsx
@@ -1,11 +1,12 @@
 import './Nav.css'
+import NavIten from './NavIten'
 import React from 'react'
-import {Link} from 'react-router-dom'
+
 
 export default props => 
     <aside className="menu-area">
         <nav className="menu">
-        <Link to="/"> <i className="fa fa-home"></i> Início</Link>
-        <Link to="/users"><i className="fa fa-users"></i> Usuários</Link>
+        <NavIten link="/" icon="home" title="Início"/>
+        <NavIten link="/users" icon="users" title=" Usuários"/>
         </nav>
     </aside>
