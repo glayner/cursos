a0bd5afcf569e0b34f753753ca5620e23128261e
glayner <thiagoglayner_tg@hotmail.com>
2019-10-04 20:34:19 -0300

    OmniStack 9

---
+++ rocketseat/OmniStack-9/mobile/src/pages/List.js
@@ -0,0 +1,73 @@
+import React, { useState, useEffect } from 'react';
+import socketio from 'socket.io-client';
+import { SafeAreaView, TouchableOpacity, Alert, ScrollView, AsyncStorage, Image, StyleSheet } from 'react-native';
+
+import SpotList from '../components/SpotList';
+
+import logo from '../assets/logo.png';
+
+export default function List({ navigation }) {
+    const [techs, setTechs] = useState([]);
+    useEffect(() => {
+        AsyncStorage.getItem('user').then(user_id => {
+            const socket = socketio('http://192.168.25.29:3333', {
+                query: { user_id }
+            })
+            socket.on('booking_response', booking => {
+                Alert.alert(`Sua reserva em ${booking.spot.company} em ${booking.date} foi ${booking.approved ? 'APROVADA' : 'REJEITADA'} `);
+            })
+        })
+    }, []);
+
+    useEffect(() => {
+        AsyncStorage.getItem('techs').then(storagedTechs => {
+            const techsArray = storagedTechs.split(',').map(tech => tech.trim());
+            setTechs(techsArray);
+        })
+    }, [])
+
+    async function handleLogin() {
+        await Alert.alert(
+            'LOGOFF',
+            'Tem certeza que deslogar do sistema?',
+            [
+                {
+                    text: 'Cancel',
+                    onPress: () => console.log('Cancel Pressed'),
+                    style: 'cancel',
+                },
+                {
+                    text: 'OK', onPress: () => {
+                         AsyncStorage.setItem('user', '');
+                        navigation.navigate('Login');
+                    }
+                },
+            ],
+            { cancelable: false },
+        );
+    }
+
+    return (
+        <SafeAreaView style={styles.container}>
+            <TouchableOpacity onPress={handleLogin}>
+                <Image style={styles.logo} source={logo} />
+            </TouchableOpacity>
+            <ScrollView>
+                {techs.map(tech => <SpotList key={tech} tech={tech} />)}
+            </ScrollView>
+        </SafeAreaView>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        paddingBottom: 30,
+    },
+    logo: {
+        height: 32,
+        resizeMode: "contain",
+        alignSelf: 'center',
+        marginTop: 30,
+    },
+})
